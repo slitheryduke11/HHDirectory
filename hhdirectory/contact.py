9d87412c55f911265fb4cd47fd75023a46222f95
Hedguhar Domínguez <hedguhar2511@gmail.com>
2023-10-25 00:30:47 -0600

    feat: add logic and templates for contact creation

--- hhdirectory/contact.py
+++ hhdirectory/contact.py
@@ -17,3 +17,36 @@ def index():
         " ORDER BY name ASC"
     ).fetchall()
     return render_template("contact/index.html", contacts=contacts)
+
+
+@bp.route("/create/", methods=("GET", "POST"))
+def create():
+    """Create a new contact."""
+    # Create contact
+    if request.method == "POST":
+        # Get data from form
+        name = request.form["name"]
+        lastname = request.form["lastname"]
+        phone = request.form["phone"]
+        email_username = request.form["email_username"]
+        email_domain = request.form["email_domain"]
+        email = None
+        # Check if necessary data is present
+        if not name or not lastname or not phone:
+            return render_template("contact/create.html", previous_data=request.form, error=True)
+        if email_username and email_domain:
+            email = email_username + "@" + email_domain # Format email
+        address = request.form["address"]
+        if 'favorite' in request.form:
+            favorite = 1 if request.form["favorite"] else 0
+        # Add contact to database
+        db = get_db()
+        db.execute(
+            "INSERT INTO contact (name, lastname, phone, email, address, favorite)"
+            " VALUES (?, ?, ?, ?, ?, ?)",
+            (name, lastname, phone, email, address, favorite),
+        )
+        db.commit()
+        return redirect(url_for("contact.index"))
+    # Show form to create contact
+    return render_template("contact/create.html")
