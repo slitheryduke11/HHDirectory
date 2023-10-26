5995fc1d20e1d1d2545ec4f9cf3d11481fb928dc
Hedguhar Domínguez <hedguhar2511@gmail.com>
2023-10-26 11:11:05 -0600

    feat: implement contact editing and deletion

--- hhdirectory/contact.py
+++ hhdirectory/contact.py
@@ -80,3 +80,61 @@ def detail(contact_id):
     if not contact:
         return redirect(url_for("contact.index"))
     return render_template("contact/detail.html", contact=contact)
+
+
+@bp.route("/contact/<contact_id>/delete/", methods=("POST",))
+def delete(contact_id):
+    """Delete a contact."""
+    # Remove contact from database
+    db = get_db()
+    db.execute(
+        "DELETE FROM contact"
+        " WHERE id = ?",
+        (contact_id,),
+    )
+    db.commit()
+    return redirect(url_for("contact.index"))
+
+
+@bp.route("/contact/<contact_id>/edit/", methods=("GET", "POST"))
+def edit(contact_id):
+    """Edit a new contact."""
+    # Get contact from database
+    db = get_db()
+    contact = db.execute(
+        "SELECT id, name, lastname, phone, email, address, favorite"
+        " FROM contact"
+        " WHERE id = ?",
+        (contact_id,),
+    ).fetchone()
+    # Check if contact exists
+    if not contact:
+        return redirect(url_for("contact.index"))
+    # Edit contact
+    if request.method == "POST":
+        # Get data from form
+        name = request.form["name"]
+        lastname = request.form["lastname"]
+        phone = request.form["phone"]
+        email_username = request.form["email_username"]
+        email_domain = request.form["email_domain"]
+        email = None
+        # Check if necessary data is present
+        if not name or not lastname or not phone:
+            return render_template("contact/edit.html", previous_data=request.form, contact=contact, error=True)
+        if email_username and email_domain:
+            email = email_username + "@" + email_domain # Format email
+        address = request.form["address"] if request.form["address"] else None
+        favorite = 1 if 'favorite' in request.form else 0
+        # Add contact to database
+        db = get_db()
+        db.execute(
+            "UPDATE contact "
+            " SET name = ?, lastname= ?, phone= ?, email= ?, address= ?, favorite= ?"
+            " WHERE id = ?",
+            (name, lastname, phone, email, address, favorite, contact_id),
+        )
+        db.commit()
+        return redirect(url_for("contact.detail", contact_id=contact_id))
+    # Show form to edit contact
+    return render_template("contact/edit.html", contact=contact)
